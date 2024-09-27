77187427bf1feabaf24a089d9a17df7fc8edefa0
deadprogram <ron@hybridgroup.com>
2024-09-27 14:44:06 +0200

    release 0.1.0-pre2
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- wasmvision.rb
+++ wasmvision.rb
@@ -1,14 +1,14 @@
 class Wasmvision < Formula
     desc "wasmVision gets you up and running with computer vision."
     homepage "https://wasmvision.com/"
-    version "0.1.0-pre1"
+    version "0.1.0-pre2"
 
     depends_on "opencv"
   
     on_macos do
         on_arm do
             url "https://github.com/wasmvision/wasmvision/releases/download/v#{version}/wasmvision-macos-arm64.tar.gz"
-            sha256 "a2bc7c33fe25ec0a6162179c36a5d5a97e748043e81773b82218dbb7f7e7e6f5"
+            sha256 "2b77531c8a3a90dfea95bebf782b678546e0bb01dde9c87a6d795d7ec5f556b1"
         end
     end
 
