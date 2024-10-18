687f9308eba59792fbdfa723a892fe2870f644da
deadprogram <ron@hybridgroup.com>
2024-10-18 20:02:58 +0200

    wasmvision: better error message for Intel macs
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- wasmvision.rb
+++ wasmvision.rb
@@ -2,16 +2,16 @@ class Wasmvision < Formula
     desc "wasmVision gets you up and running with computer vision."
     homepage "https://wasmvision.com/"
     version "0.1.0-beta2"
-
-    depends_on "opencv"
   
     on_macos do
         on_arm do
+            depends_on "opencv"
+
             url "https://github.com/wasmvision/wasmvision/releases/download/v#{version}/wasmvision-macos-arm64.tar.gz"
             sha256 "35ed65926bef12ebd37a1ef789e3157059756e9009812fadc4f3c3fcbeb56d37"
         end
         on_intel do
-            odie "wasmVision homebrew installer does not currently support macOS on Intel processors."
+            odie "wasmVision homebrew installer does not currently support macOS on Intel processors. You can however install from source. Go to https://github.com/wasmvision/wasmvision/blob/main/DEVELOPMENT.md"
         end
     end
 
