0abf23ae305112b0af70576ba0a4b321112b932c
deadprogram <ron@hybridgroup.com>
2025-03-31 11:16:29 +0200

    Release 0.3.1
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- wasmvision.rb
+++ wasmvision.rb
@@ -1,14 +1,14 @@
 class Wasmvision < Formula
-    desc "wasmVision gets you up and running with computer vision."
-    homepage "https://wasmvision.com/"
-    version "0.3.0"
+    desc "wasmVision gets you up and running with computer vision using WebAssembly."
+    homepage "http://wasmvision.com/"
+    version "0.3.1"
   
     on_macos do
         on_arm do
             depends_on "opencv"
 
             url "https://github.com/wasmvision/wasmvision/releases/download/v#{version}/wasmvision-macos-arm64.tar.gz"
-            sha256 "00c60378cec30d8f5e1f1d04f1fcf0174eb64b1b3f19e7c04ea5ce82c99ea0b6"
+            sha256 "5c02c68ce715b7a6789e7622856c698fc7537ee9a699f942192f799c55a67b11"
         end
         on_intel do
             odie "wasmVision homebrew installer does not currently support macOS on Intel processors. You can however install from source. Go to https://github.com/wasmvision/wasmvision/blob/main/docs/development.md"
